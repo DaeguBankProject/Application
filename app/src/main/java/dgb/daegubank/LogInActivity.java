1ed78f9794b62158f20f7767f067b860ee019f38
MSChoi <yuuhio9681@gmail.com>
2018-10-28 16:01:47 +0900

    Network Message

--- app/src/main/java/dgb/daegubank/LogInActivity.java
+++ app/src/main/java/dgb/daegubank/LogInActivity.java
@@ -21,4 +21,19 @@ public class LogInActivity extends AppCompatActivity{
         signInButton = (Button)findViewById(R.id.SignInBtn);
         signUpButton = (Button)findViewById(R.id.SignUpBtn);
     }
+
+    /*
+        HttpURLConnection serverConnection;
+        serverConnection = .........;
+        JSONObject requestObject = new JSONObject();
+        requestObject.put("type", "sign_in");
+        requestObject.put("id", $(DATA));
+        ...
+        byte[] postDataBytes = requestObject.toString().getBytes(NetworkInterface.ENCODE);
+        ...
+        serverConnection.setRequestMethod("POST");
+        serverConnection.setRequestProperty("Content-Type", "application/json");
+        serverConnection.SetDoOutput(true);
+        serverConnection.getOutputStream().write(postDataBytes);
+    */
 }
